8825addbb0bdbfae6699c08861fa10103bdd852d
SergeChurkin-ca <sergechurkin@yahoo.com>
2021-09-08 00:20:32 -0400

    functionality: player control-one file at at time; form submition and validation

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,51 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 export default function Contact() {
+
+  useEffect(() => {
+  window.addEventListener("DOMContentLoaded", function() {
+    // get the form elements defined in your form HTML above
+
+    let form = document.getElementById("my-form");
+    let button = document.getElementById("my-form-button");
+    let status = document.getElementById("my-form-status");
+
+    // Success and Error functions for after the form is submitted
+
+    function success() {
+        form.reset();
+        button.style = "display: none ";
+        status.innerHTML = "Thank you for your message!";
+        document.getElementById("button-reset").style.display = "none";
+    }
+
+    function error() {
+        status.innerHTML = "Oops! There was a problem. Please check your inputs.";
+    }
+
+    form.addEventListener("submit", function(ev) {
+        ev.preventDefault();
+        let data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+});
+
+function ajax(method, url, data, success, error) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
+})
+
   return (
     <section id="contact" className="stayintouch">
       <div className="text-wrapper">
--- src/components/Tracks/Tracks.js
+++ src/components/Tracks/Tracks.js
@@ -1,54 +1,90 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
 export default function Tracks() {
-    return (
-              <section id="tracks" className="tracks">
-                <div className="content">
-                    <div className="text-wrapper">
-                        <h2>Popular tracks</h2>
-                        <h3>Here are our the most inspiring compositions</h3>
-                    </div>
-                    <div className="audio-wrapper">
-                        <figure>
-                            <figcaption>Dream</figcaption>
-                            <audio controls src="./assets/audio/jam_bubble_band_-_dreems.mp3">
-                                 Your browser does not support the
-                            <code>audio</code> element.
-                             </audio>
 
-                        </figure>
-                        <figure>
-                            <figcaption>King Size Johns</figcaption>
-                            <audio controls src="./assets/audio/jam_bubble_band_-_king_size_johns.mp3">
-                                 Your browser does not support the
-                            <code>audio</code> element.
-                             </audio>
+     
+     // playing only one audio/video file at a time
+  useEffect(() => {
+    function onlyPlayOneIn(container) {
+      container.addEventListener(
+        "play",
+        function (event) {
+          const audio_elements = document.getElementsByTagName("audio");
+          for (let i = 0; i < audio_elements.length; i++) {
+            const audio_element = audio_elements[i];
+            if (audio_element !== event.target) {
+              audio_element.pause();
+            }
+          }
+        },
+        true
+      );
+    }
+    document.addEventListener("DOMContentLoaded", function () {
+      onlyPlayOneIn(document.body);
+    });
+  });
 
-                        </figure>
-                        <figure>
-                            <figcaption>Sny</figcaption>
-                            <audio controls src="./assets/audio/jam_bubble_band_-_sni_mastering_sergej_galamaga.mp3">
-                                 Your browser does not support the
-                            <code>audio</code> element.
-                             </audio>
-                        </figure>
-                        <figure>
-                            <figcaption>Sorry</figcaption>
-                            <audio controls src="./assets/audio/jam_bubble_band_-_sorry.mp3">
-                                 Your browser does not support the
-                            <code>audio</code> element.
-                             </audio>
-                        </figure>
-                        <figure>
-                            <figcaption>Tato Propav</figcaption>
-                            <audio controls src="./assets/audio/jam_bubble_band_-_tato_popav.mp3">
-                                 Your browser does not support the
-                            <code>audio</code> element.
-                             </audio>
-                        </figure>
-                    </div>
-                </div>
-            
-            </section>
-    )
+  return (
+    <section id="tracks" className="tracks">
+      <div className="content">
+        <div className="text-wrapper">
+          <h2>Popular tracks</h2>
+          <h3>Here are our the most inspiring compositions</h3>
+        </div>
+        <div className="audio-wrapper">
+          <figure>
+            <figcaption>Dream</figcaption>
+            <audio
+              controls
+              src="https://serge-web.com/bubblejamband/assets/audio/jam_bubble_band_-_dreems.mp3"
+            >
+              Your browser does not support the
+              <code>audio</code> element.
+            </audio>
+          </figure>
+          <figure>
+            <figcaption>King Size Johns</figcaption>
+            <audio
+              controls
+              src="https://serge-web.com/bubblejamband/assets/audio/jam_bubble_band_-_king_size_johns.mp3"
+            >
+              Your browser does not support the
+              <code>audio</code> element.
+            </audio>
+          </figure>
+          <figure>
+            <figcaption>Sny</figcaption>
+            <audio
+              controls
+              src="https://serge-web.com/bubblejamband/assets/audio/jam_bubble_band_-_sni_mastering_sergej_galamaga.mp3"
+            >
+              Your browser does not support the
+              <code>audio</code> element.
+            </audio>
+          </figure>
+          <figure>
+            <figcaption>Sorry</figcaption>
+            <audio
+              controls
+              src="https://serge-web.com/bubblejamband/assets/audio/jam_bubble_band_-_sorry.mp3"
+            >
+              Your browser does not support the
+              <code>audio</code> element.
+            </audio>
+          </figure>
+          <figure>
+            <figcaption>Tato Propav</figcaption>
+            <audio
+              controls
+              src="https://serge-web.com/bubblejamband/assets/audio/jam_bubble_band_-_tato_popav.mp3"
+            >
+              Your browser does not support the
+              <code>audio</code> element.
+            </audio>
+          </figure>
+        </div>
+      </div>
+    </section>
+  );
 }
